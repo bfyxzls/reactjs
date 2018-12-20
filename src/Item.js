2da8d077b4f308f2dd39a93718b0edb73be41883
zhangzl <zhangzl@i-counting.cn>
2018-12-20 19:42:30 +0800

    添加别名

--- src/Item.js
+++ src/Item.js
@@ -1,8 +1,18 @@
 import React from "react";
+import "./Item.css";
 
 export default class Item extends React.Component {
   render() {
-    return <li>{this.props.item}</li>;
+    return (
+      <li className="list">
+      <span>
+        {this.props.item.name}
+        </span>
+        <span>
+        {this.props.item.age}
+        </span>
+      </li>
+    );
     // props是从父模块传入的
   }
 }
--- src/ItemList.js
+++ src/ItemList.js
@@ -1,9 +1,13 @@
 import React from "react";
-import Item from "./Item";
+import { item } from "./Item";
 
 export default class ItemList extends React.Component {
   render() {
-    let data = ["一", "二", 3, 4, 5];//方法内部定义
-    return data.map((val, index) => <Item key={index} item={val} />);
+    let data = [
+      { name: "zzl", age: 35 },
+      { name: "zql", age: 39 },
+      { name: "zhz", age: 9 }
+    ]; //方法内部定义
+    return data.map((val, index) => <item key={index} item={val} />);
   }
 }
